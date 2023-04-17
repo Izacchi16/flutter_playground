cd4d05d07c9facb4029f0ccdbc792dd9df46670b
Izacchi16 <sakuya1202@icloud.com>
2023-04-17 15:13:37 +0900

    https://flutter.ctrnost.com/tutorial/tutorial05/

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp()); /*1*/
+void main() => runApp(const MyApp()); /*1*/
 
 class MyApp extends StatelessWidget {
-  /*2*/
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
