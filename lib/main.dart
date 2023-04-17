0bf35be03228b899b9a4f0ca6677c90f6bec121e
Izacchi16 <sakuya1202@icloud.com>
2023-04-17 14:35:31 +0900

    外部パッケージ:https://flutter.ctrnost.com/tutorial/tutorial04/

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp()); /*1*/
 
-class MyApp extends StatelessWidget {/*2*/
+class MyApp extends StatelessWidget {
+  /*2*/
   @override
-  Widget build(BuildContext context) { /*3*/
-    return MaterialApp(  /*4*/
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold( /*5*/
-        appBar: AppBar( /*6*/
+      home: Scaffold(
+        appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center( /*7*/
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
